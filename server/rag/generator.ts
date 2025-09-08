ae3166786ecb5c2a354278c8895622f805183ef9
Shashi prakash singh <singhshashi09871@gmail.com>
2025-09-08 20:18:47 +0530

    Update generator.ts

--- server/rag/generator.ts
+++ server/rag/generator.ts
@@ -5,6 +5,6 @@ const model = new ChatOpenAI({ apiKey: process.env.OPENAI_API_KEY });
 
 export const generateAnswer = async (query: string, context: string) => {
   const prompt = `Context: ${context}\n\nQuestion: ${query}`;
-  const response = await model.invoke([new HumanMessage(prompt)]);
+  const response = await model.invoke([new HumanMessage(prompt)]); 
   return response.content;
 };
