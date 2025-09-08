c90a5f52df82217105727d354fa51bb50e3014b5
Shashi prakash singh <singhshashi09871@gmail.com>
2025-09-08 20:19:59 +0530

    Update retriever.ts

--- server/rag/retriever.ts
+++ server/rag/retriever.ts
@@ -2,5 +2,5 @@ import { MemoryVectorStore } from "@langchain/core/vectorstores/memory";
 import { embedder } from "./embedder";
 import { loadDocs } from "./loader"; 
 
-export const retriever = await MemoryVectorStore.fromDocuments(loadDocs(), embedder);  
+export const retriever = await MemoryVectorStore.fromDocuments(loadDocs(), embedder);   
  
