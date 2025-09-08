5c1bfc8196842111b2986ee56fcc4bfa262b56ef
Shashi prakash singh <singhshashi09871@gmail.com>
2025-09-08 20:19:13 +0530

    Update loader.ts

--- server/rag/loader.ts
+++ server/rag/loader.ts
@@ -2,6 +2,7 @@ import { Document } from "@langchain/core/documents";
 export const loadDocs = (): Document[] => {
   return [
     new Document({ pageContent: "Delhi is the capital of India." }),
-    new Document({ pageContent: "React is a JavaScript library for building UIs." }),
+    new Document({ pageContent: "React is a JavaScript library for building UIs." }), 
+    
   ];
 };
