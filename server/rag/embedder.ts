ad19df7f667304da13898b86508d847b09613428
Shashi prakash singh <singhshashi09871@gmail.com>
2025-09-24 19:14:07 +0530

    Update embedder.ts

--- server/rag/embedder.ts
+++ server/rag/embedder.ts
@@ -1,3 +1,3 @@
-     import { OpenAIEmbeddings } from "@langchain/openai";
+      import { OpenAIEmbeddings } from "@langchain/openai";
 export const embedder = new OpenAIEmbeddings({ apiKey: process.env.OPENAI_API_KEY });
   
